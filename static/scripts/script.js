5a3059869ffcedfdd4cce19c24ce406e96677d0a
da_deef <da_deef@mac.com>
2012-12-28 15:24:44 +0100

    pwd-protected post page, couchtypes to validate doc

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,10 +2,18 @@ var $ = require('jquery/core');
 var session = require('session');
 var auth_controls = require('duality/contrib/session/controls');
 require('duality/contrib/session/events');
+var helpers = require('lib/helpers');
+var session = require("session");
 
 $(document).ready(function() {
 	console.log('doc.ready');
 	
 	//Auth
 	auth_controls.bind();
+});
+
+
+session.on('change', function (userCtx) {
+   	console.log('session.change');
+	window.location.reload();
 });
