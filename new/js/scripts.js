272dfcd262f3e08bc670effd62ba7292585b9234
Pierre Van der Eecken <pierre@yaska.eu>
2014-05-01 19:50:27 +0200

    responsive

---
+++ new/js/scripts.js
@@ -0,0 +1,26 @@
+// Create the dropdown base
+$('<select class="mobile" />').appendTo("nav");
+
+// Create default option "Go to..."
+$("<option />", {
+   "selected": "selected",
+   "value"   : "",
+   "text"    : "Go to..."
+}).appendTo("nav select");
+
+// Populate dropdown with menu items
+$(".menu a").each(function() {
+ var el = $(this);
+ $("<option />", {
+     "value"   : el.attr("href"),
+     "text"    : el.text()
+ }).appendTo("nav select");
+});
+
+$("nav select").change(function() {
+  window.location = $(this).find("option:selected").val();
+});
+
+$('.carousel').carousel({
+  interval: 2000
+});
