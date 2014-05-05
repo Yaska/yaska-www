6cb565c72d65f0cc50a75be95a047885856120d1
Pierre Van der Eecken <pierre@yaska.eu>
2014-05-05 11:07:46 +0200

    mobile navigation

--- new/js/scripts.js
+++ new/js/scripts.js
@@ -1,23 +1,24 @@
 // Create the dropdown base
-$('<select class="mobile" />').appendTo("nav");
+$('<select class="mobile" />').appendTo(".container header .nav .menu");
 
 // Create default option "Go to..."
 $("<option />", {
    "selected": "selected",
    "value"   : "",
-   "text"    : "Go to..."
-}).appendTo("nav select");
+   "text"    : "navigation"
+}).appendTo(".nav select");
 
 // Populate dropdown with menu items
 $(".menu a").each(function() {
- var el = $(this);
- $("<option />", {
-     "value"   : el.attr("href"),
-     "text"    : el.text()
- }).appendTo("nav select");
+  var el = $(this);
+  console.log(el.attr("href"));
+  $("<option />", {
+      "value"   : el.attr("href"),
+      "text"    : el.text()
+  }).appendTo(".nav select");
 });
 
-$("nav select").change(function() {
+$(".nav select").change(function() {
   window.location = $(this).find("option:selected").val();
 });
 
