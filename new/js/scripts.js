d0d4e8152c302a6d7d510c7fc56dc30db0a8857c
Pierre Van der Eecken <pierre@yaska.eu>
2014-05-06 14:42:35 +0200

    finetuning

--- new/js/scripts.js
+++ new/js/scripts.js
@@ -25,3 +25,31 @@ $(".nav select").change(function() {
 $('.carousel').carousel({
   interval: 2000
 });
+
+$('a[href*=#]:not([href=#])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top - 100
+      }, 1000);
+      return false;
+    }
+  }
+});
+
+/*
+$(window).load(function(){
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 260,
+    itemMargin: 0,
+    //pausePlay: true,
+    start: function(slider){
+      $('body').removeClass('loading');
+    }
+  });
+});
+*/
